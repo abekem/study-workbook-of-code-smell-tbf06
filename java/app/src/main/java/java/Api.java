74dc390714143911e7806b7a239e003444faa509
abekem <nek.7180@gmail.com>
2025-05-19 07:13:30 +0000

    Fix package declaration in Api and ApiConfig classes

--- java/app/src/main/java/java/Api.java
+++ java/app/src/main/java/java/Api.java
@@ -1,4 +1,4 @@
-package main.java.java;
+package java;
 
 public class Api {
     private ApiConfig conf;
--- java/app/src/main/java/java/ApiConfig.java
+++ java/app/src/main/java/java/ApiConfig.java
@@ -1,14 +1,18 @@
-package main.java.java;
+package java;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
 
 import netscape.javascript.JSObject;
 
 public class ApiConfig {
     private static final String CONF_PATH = "/api_config.json";
 
-    private String conf;
+    private Map<String, String> conf;
 
     public ApiConfig() {
         try (InputStream is = getClass().getResourceAsStream(CONF_PATH);
@@ -17,10 +21,23 @@ public class ApiConfig {
             System.out.println(br.readLine());
             // String json = File.readAllText(CONF_PATH);
             String json = br.lines().collect();
+            
+            // JSONをパースしてMapに変換
+            ObjectMapper objectMapper = new ObjectMapper();
+            // JSON文字列をMapに変換
+            this.conf = mapper.readValue(json, Map.class);
             this.conf = JSObject.parse(json);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    public String getId() {
+        return conf.get("id");
+    }
+
+    public String getPassword() {
+        return conf.get("password");
     }
 }
